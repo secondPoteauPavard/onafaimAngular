2d8f8ded5f53ccfcdf99c0ffe88613494eb7db8a
unknown <thibautlesueur@hotmail.fr>
2020-02-19 17:46:14 +0100

    mercredi soir

--- src/app/services/panier.service.ts
+++ src/app/services/panier.service.ts
@@ -11,13 +11,15 @@ export class PanierService {
   private url: string = 'http://localhost:8080/onafaim/panier/2'
   private headers: HttpHeaders;
   private options: object;
+  private ligneCommande: any = {};
+  private commande: any = {};
 
   constructor(private http: HttpClient) { }
 
   private authentication() {
     this.headers = new HttpHeaders( {
       'Content-Type': 'application/json',
-      'Authorization': 'Basic ' + sessionStorage.getItem('user')
+      'Authorization': 'Basic ' + btoa('system:system')
     });
     this.options = {headers: this.headers};
   }
@@ -32,9 +34,20 @@ export class PanierService {
     return this.http.delete(this.url + '/' + nom, this.options);
   }*/
 
-  public EnregistrerEnBase() {
+
+  public EnregistrerCommandeEnBase(monPanierList) {
     this.authentication();
-    // return this.http.get(this.url, this.options);
+    this.commande = `{"compte":{"id":${sessionStorage.getItem('id')}}}`;
+    console.log(this.commande);
+    return this.http.post('http://localhost:8080/onafaim/commande/save', this.commande, this.options);
   }
 
+  // Comment récupérer l'id de la commande auto généré ci dessus ??   --> A intégrer en dessous
+
+  public EnregistrerLigneCommandeEnBase(monPanierList) {
+    this.authentication();
+    this.ligneCommande = `{"qte": ${monPanierList.qte},"id": {"commande":{"id": 105},"produit": {"id": ${monPanierList.produit.id}}}`;
+    console.log(this.ligneCommande);
+    return this.http.post('http://localhost:8080/onafaim/commande/save', this.ligneCommande, this.options);
+  }
 }
